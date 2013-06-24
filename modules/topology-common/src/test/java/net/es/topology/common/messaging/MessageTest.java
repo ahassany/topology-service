7da31fba04ddea71159789c135f44e187ba7cf3d
Ahmed El-Hassany <ahassany@indiana.edu>
2013-06-24 15:50:57 -0700

    update the messaging schema to include correct body

--- modules/topology-common/src/test/java/net/es/topology/common/messaging/MessageTest.java
+++ modules/topology-common/src/test/java/net/es/topology/common/messaging/MessageTest.java
@@ -1,25 +1,18 @@
 package net.es.topology.common.messaging;
 
+import org.apache.commons.io.IOUtils;
+import org.hamcrest.CoreMatchers;
 import org.junit.Assert;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
-
-
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.JAXBException;
-import javax.xml.bind.Marshaller;
-import javax.xml.bind.Unmarshaller;
-import java.io.InputStreamReader;
-import java.io.StringWriter;
-
-import org.apache.commons.io.IOUtils;
-import org.hamcrest.CoreMatchers;
-
 import org.ogf.schemas.nml._2013._05.base.NodeType;
+import org.ogf.schemas.nml._2013._05.base.ObjectFactory;
 import org.ogf.schemas.nsi._2013._09.messaging.Message;
 
+import javax.xml.bind.*;
+import java.io.InputStreamReader;
+import java.io.StringWriter;
 
 
 /**
@@ -28,28 +21,35 @@ import org.ogf.schemas.nsi._2013._09.messaging.Message;
 
 @RunWith(JUnit4.class)
 public class MessageTest {
+    public final static String jaxb_bindings = "org.ogf.schemas.nsi._2013._09.topology:org.ogf.schemas.nsi._2013._09.messaging:org.ogf.schemas.nml._2013._05.base";
+
     @Test
-    public void testMessageCreate() throws JAXBException{
+    public void testMessageCreate() throws JAXBException {
         final String nodeName = "Node1";
 
         // First create a message
         Message msg = new Message();
-        NodeType node = new NodeType();
-        node.setName(nodeName);
-        msg.setBody(node);
+        Message.Body body = new Message.Body();
+        JAXBElement<NodeType> node = new ObjectFactory().createNode(new NodeType());
+        node.getValue().setName(nodeName);
+        body.setAny(node);
+        msg.setBody(body);
 
         // make sure it marshals correctly
-        JAXBContext context = JAXBContext.newInstance("org.ogf.schemas.nsi._2013._09.messaging:org.ogf.schemas.nml._2013._05.base");
+        JAXBContext context = JAXBContext.newInstance(jaxb_bindings);
         Marshaller marshaller = context.createMarshaller();
         StringWriter out = new StringWriter();
         marshaller.marshal(msg, out);
+
+        System.out.println(out);
 
         // Read it again
         Unmarshaller um = context.createUnmarshaller();
         Message umsg = (Message) um.unmarshal(new InputStreamReader(IOUtils.toInputStream(out.toString())));
 
         // Make sure we have the same value
-        Assert.assertThat(umsg.getBody(), CoreMatchers.instanceOf(NodeType.class));
-        Assert.assertEquals(((NodeType)umsg.getBody()).getName(), nodeName);
+        Message.Body ubody = umsg.getBody();
+        Assert.assertThat(ubody.getAny(), CoreMatchers.instanceOf(javax.xml.bind.JAXBElement.class));
+        Assert.assertEquals(((JAXBElement<NodeType>) ubody.getAny()).getValue().getName(), nodeName);
     }
 }
