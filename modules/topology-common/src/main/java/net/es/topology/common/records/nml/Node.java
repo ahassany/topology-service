139a26db72021deee1909c32742cff728ec7865d
Ahmed El-Hassany <a.hassany@gmail.com>
2013-06-27 16:19:03 -0700

    add comments to sLS Node

--- modules/topology-common/src/main/java/net/es/topology/common/records/nml/Node.java
+++ modules/topology-common/src/main/java/net/es/topology/common/records/nml/Node.java
@@ -1,7 +1,7 @@
 package net.es.topology.common.records.nml;
 
-import net.es.topology.common.records.nml.keys.ReservedValues;
 import net.es.topology.common.records.nml.keys.ReservedKeys;
+import net.es.topology.common.records.nml.keys.ReservedValues;
 
 import java.util.List;
 
@@ -9,38 +9,88 @@ import java.util.List;
  * @author <a href="mailto:a.hassany@gmail.com">Ahmed El-Hassany</a>
  */
 public class Node extends NetworkObject {
-    public Node(){
+
+    public Node() {
         super(ReservedValues.RECORD_TYPE_NODE);
     }
 
+    /**
+     * To allow extending this class with custom record type
+     *
+     * @param recordType
+     */
+    public Node(String recordType) {
+        super(recordType);
+    }
+
+    /**
+     * This defines that the related Network Object has an inbound Port or PortGroup object.
+     *
+     * @return list of the URNs of inbound ports
+     */
     public List<String> getHasInboundPort() {
         return (List<String>) this.getValue(ReservedKeys.RECORD_RELATION_HAS_INBOUND_PORT);
     }
 
+    /**
+     * This defines that the related Network Object has an inbound Port or PortGroup object.
+     *
+     * @param ports list of the URNs of inbound ports
+     */
     public void setHasInboundPort(List<String> ports) {
         this.add(ReservedKeys.RECORD_RELATION_HAS_INBOUND_PORT, ports);
     }
 
+    /**
+     * This defines that the related Network Object has an outbound Port or PortGroup object.
+     *
+     * @return list of the URNs of outbound ports
+     */
     public List<String> getHasOutboundPort() {
         return (List<String>) this.getValue(ReservedKeys.RECORD_RELATION_HAS_OUTBOUND_PORT);
     }
 
+    /**
+     * This defines that the related Network Object has an outbound Port or PortGroup object.
+     *
+     * @param ports list of the URNs of outbound ports
+     */
     public void setHasOutboundPort(List<String> ports) {
         this.add(ReservedKeys.RECORD_RELATION_HAS_OUTBOUND_PORT, ports);
     }
 
+    /**
+     * Node related to SwitchingService, describing a switching ability of that Node.
+     *
+     * @return List of SwitchingServices URNs
+     */
     public List<String> getHasService() {
         return (List<String>) this.getValue(ReservedKeys.RECORD_RELATION_HAS_SERVICE);
     }
 
+    /**
+     * Node related to SwitchingService, describing a switching ability of that Node
+     *
+     * @param services List of SwitchingServices URNs
+     */
     public void setHasService(List<String> services) {
         this.add(ReservedKeys.RECORD_RELATION_HAS_SERVICE, services);
     }
 
+    /**
+     * isAlias is a relation to another Node describe that one can be used as the alias of another.
+     *
+     * @return list of the URNs of the other Nodes.
+     */
     public List<String> getIsAlias() {
         return (List<String>) this.getValue(ReservedKeys.RECORD_RELATION_IS_ALIAS);
     }
 
+    /**
+     * isAlias is a relation to another Node describe that one can be used as the alias of another.
+     *
+     * @param aliases list of the URNs of the other Nodes.
+     */
     public void setIsAlias(List<String> aliases) {
         this.add(ReservedKeys.RECORD_RELATION_IS_ALIAS, aliases);
     }
